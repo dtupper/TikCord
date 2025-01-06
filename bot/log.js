60fa8c41ca24b032f082d74cdd026376301c2a2f
GreenCobalt <34927146+GreenCobalt@users.noreply.github.com>
2025-01-06 01:54:13 -0500

    Revert "add file logging"
    This reverts commit d66206d178861379749471e63df556c0e8f907a3.

--- bot/log.js
+++ bot/log.js
@@ -1,32 +1,21 @@
-const fs = require('fs');
-
 let shardId = undefined;
-
 function init(sId) {
     shardId = sId;
-
-    if (!fs.existsSync('/var/log/tikcord')){
-        fs.mkdirSync('/var/log/tikcord');
-    }
 }
 
 function error(message) {
-    fs.writeFileSync(`/var/log/tikcord/${shardId}.log`, `[${Date.now()}] [ERR] ${message}`);
     console.log(`[${shardId}] [${Date.now()}] [ERR] ${message}`);
 }
 
 function warn(message) {
-    fs.writeFileSync(`/var/log/tikcord/${shardId}.log`, `[${Date.now()}] [WRN] ${message}`);
     console.log(`[${shardId}] [${Date.now()}] [WRN] ${message}`);
 }
 
 function info(message) {
-    fs.writeFileSync(`/var/log/tikcord/${shardId}.log`, `[${Date.now()}] [INF] ${message}`);
     console.log(`[${shardId}] [${Date.now()}] [INF] ${message}`);
 }
 
 function debug(message) {
-    fs.writeFileSync(`/var/log/tikcord/${shardId}.log`, `[${Date.now()}] [DBG] ${message}`);
     console.log(`[${shardId}] [${Date.now()}] [DBG] ${message}`);
 }
 
